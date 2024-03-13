37daf048b8a2aa537ae655b43ea54a99c01fe4bb
luluzinha07 <162439992+luluzinha07@users.noreply.github.com>
2024-03-13 08:34:59 -0300

    Add files via upload

---
+++ atividade/calculadora.py
@@ -0,0 +1,16 @@
+numero1=float(input("Digite o primeiro numero: "))
+numero2=float(input("Digite o primeiro numero: "))
+
+
+soma=numero1+numero2
+subtracao=numero1-numero2
+divisao=numero1/numero2
+multiplicacao=numero1*numero2
+
+print("A soma dos numero é igual a",soma,)
+print("A subtração dos numero é igual a",subtracao,)
+print("A divisão dos numero é igual a",divisao,)
+print("A multiplicação dos numero é igual a",multiplicacao,)
+
+
+
